88324e85dc03d34d9fbc211098268664a62e17d5
小方敏輝 <pro.ogata@karabiner.tech>
2019-10-16 12:25:14 +0900

    update

--- keyboards/fortitude60/keymaps/original/keymap.c
+++ keyboards/fortitude60/keymaps/original/keymap.c
@@ -33,15 +33,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+-------------+------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   [  |   ]  |   N  |   M  |   ,  |   .  |   /  |Shift |
  * `-------------+------+------+------+------+------+------+------+------+------+------+-------------'
- *               |  GUI |  Alt |  Esc |Lower |Space |Enter |Raise | Bksp |  Alt |  GUI |
+ *               |  GUI |  Alt |Space |Lower |  Esc |Enter |Raise | Bksp |  Alt |  GUI |
  *               `---------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT( \
-  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,   KC_5,                   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, \
-  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,                   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_EQL, \
-  KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,   KC_G,                   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,   KC_B, KC_LBRC, KC_RBRC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT , \
-                 KC_LGUI, KC_LALT,  KC_ESC,  LOWER,  KC_SPC, KC_ENT, RAISE, KC_BSPC,    KC_RALT, KC_RGUI\
+  KC_GRV,  KC_1, KC_2,    KC_3,    KC_4,          KC_5,                                   KC_6,                KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, \
+  KC_TAB,  KC_Q, KC_W,    KC_E,    KC_R,          KC_T,                                   KC_Y,                KC_U,    KC_I,    KC_O,    KC_P,    KC_EQL,  \
+  KC_LCTL, KC_A, KC_S,    KC_D,    KC_F,          KC_G,                                   KC_H,                KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+  KC_LSFT, KC_Z, KC_X,    KC_C,    KC_V,          KC_B,                 KC_LBRC, KC_RBRC, KC_N,                KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT ,\
+                 KC_LGUI, KC_LALT, ALT_T(KC_SPC), LT(_LOWER,KC_LANG2),  KC_ESC,  KC_ENT,  LT(_RAISE,KC_LANG1), KC_BSPC, KC_RALT, KC_RGUI\
 ),
 
 /* Lower
@@ -58,11 +58,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *               `---------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT( \
-  _______, _______, _______, _______, _______, _______,                    _______, _______, _______, _______, _______, _______,  \
-  _______, _______, S(C(KC_TAB)), KC_UP, C(KC_TAB), _______,                 _______, _______, _______, _______, _______, _______, \
-  _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______,                    _______, _______, KC_HOME, KC_PGUP, _______, _______, \
-  _______, _______, G(KC_LEFT), G(KC_UP), G(KC_RGHT), _______, _______, _______,  _______, KC_DEL , KC_END,  KC_PGDOWN, _______, _______, \
-                    _______, _______, _______, _______, _______, _______,  _______, _______, _______, _______\
+  _______, _______, _______,      _______,  _______,    _______,                    _______, _______, _______, _______,   _______, _______,  \
+  _______, _______, S(C(KC_TAB)), KC_UP,    C(KC_TAB),  _______,                    _______, _______, _______, _______,   _______, _______, \
+  _______, _______, KC_LEFT,      KC_DOWN,  KC_RGHT,    _______,                    _______, _______, KC_HOME, KC_PGUP,   _______, _______, \
+  _______, _______, G(KC_LEFT),   G(KC_UP), G(KC_RGHT), _______, _______, _______,  _______, KC_DEL , KC_END,  KC_PGDOWN, _______, _______, \
+                    _______,      _______,  _______,    _______, _______, _______,  _______, _______, _______, _______\
 ),
 
 /* Raise
