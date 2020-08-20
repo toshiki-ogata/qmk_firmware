d054b85f5711166c7c3826a02d9ccbf5e0f8b34e
小方敏輝 <ogata_toshiki@oisixradaichi.co.jp>
2020-08-20 23:23:58 +0900

    update

--- keyboards/fortitude60/keymaps/original/keymap.c
+++ keyboards/fortitude60/keymaps/original/keymap.c
@@ -31,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|             |------+------+------+------+------+------|
  * | Ctrl |   A  |   S  |   D  |   F  |   G  |             |   H  |   J  |   K  |   L  |   ;  |  "   |
  * |------+------+------+------+------+------+-------------+------+------+------+------+------+------|
- * | Shift|   Z  |   X  |   C  |   V  |   B  |   [  |   ]  |   N  |   M  |   ,  |   .  |   /  |Shift |
+ * | Shift|   Z  |   X  |   C  |   V  |   B  |   [  |   ]  |   N  |   M  |   ,  |   .  |   /  |  |   |
  * `-------------+------+------+------+------+------+------+------+------+------+------+-------------'
  *               |  GUI |  Alt |Space |Lower |  Esc |Enter |Raise | Bksp |  Alt |  GUI |
  *               `---------------------------------------------------------------------'
@@ -40,8 +40,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_GRV,  KC_1, KC_2,    KC_3,    KC_4,          KC_5,                                   KC_6,                KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, \
   KC_TAB,  KC_Q, KC_W,    KC_E,    KC_R,          KC_T,                                   KC_Y,                KC_U,    KC_I,    KC_O,    KC_P,    KC_EQL,  \
   KC_LCTL, KC_A, KC_S,    KC_D,    KC_F,          KC_G,                                   KC_H,                KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-  KC_LSFT, KC_Z, KC_X,    KC_C,    KC_V,          KC_B,                 KC_LBRC, KC_RBRC, KC_N,                KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT ,\
-                 KC_LGUI, KC_LALT, ALT_T(KC_SPC), LT(LOWER, KC_F23),  KC_ESC,  KC_ENT,  LT(RAISE, KC_F24), KC_BSPC, KC_RALT, KC_RGUI\
+  KC_LSFT, KC_Z, KC_X,    KC_C,    KC_V,          KC_B,                 KC_LBRC, KC_RBRC, KC_N,                KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS ,\
+                 KC_LGUI, KC_LALT, KC_SPC, LT(LOWER, KC_F23),  KC_ESC,  KC_ENT,  LT(RAISE, KC_F24), KC_BSPC, KC_RALT, KC_RGUI\
 ),
 
 /* Lower
